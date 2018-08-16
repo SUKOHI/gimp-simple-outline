385a7927f2e27591810cb667001fb06f5af4a840
Sukohi Kuhoh <capilano.sukohi@gmail.com>
2018-08-16 18:13:55 +0900

    Updated README.md

---
+++ FU_simple_outline.scm
@@ -0,0 +1,49 @@
+(define (FU-simple-outline
+        img
+        current-layer
+        color
+        thickness
+        feather)
+
+    (gimp-selection-layer-alpha current-layer)
+    (gimp-selection-grow img thickness)
+    (gimp-selection-feather img feather)
+
+    (let (
+            (name (car (gimp-item-get-name current-layer)))
+            (index (car (gimp-image-get-layer-position img current-layer)))
+            (width (car (gimp-image-width img)))
+            (height (car (gimp-image-height img)))
+            (new-current-layer (car (gimp-layer-copy current-layer TRUE)))
+            (outline-group (car (gimp-layer-group-new img)))
+        )
+        (gimp-item-set-name outline-group "outline-group")
+        (gimp-image-insert-layer img outline-group 0 (+ index 1))
+        (let (
+                (outline-layer (car (gimp-layer-new img width height RGBA-IMAGE (string-append name " outline") 100 LAYER-MODE-NORMAL-LEGACY)))
+            )
+            (gimp-image-remove-layer img current-layer)
+            (gimp-image-insert-layer img outline-layer outline-group 0)
+            (gimp-image-insert-layer img new-current-layer outline-group 0)
+            (gimp-palette-set-foreground color)
+            (gimp-drawable-edit-fill outline-layer FILL-FOREGROUND)
+        )
+    )
+
+    (gimp-displays-flush)
+    (gimp-selection-none img)
+
+)
+(script-fu-register "FU-simple-outline"
+    "<Image>/Script-Fu/simple-outline"
+    "Automatically add outline"
+    "Sukohi Kuhoh"
+    "Sukohi Kuhoh"
+    "2018"
+    "*"
+    SF-IMAGE    "Image"         0
+    SF-DRAWABLE "Current Layer" 0
+    SF-COLOR "Color" '(0 0 0)
+    SF-ADJUSTMENT "Thickness" '(4 1 50 1 10 0 0)
+    SF-ADJUSTMENT "Feather" '(2 1 50 1 10 0 0)
+)
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # gimp-simple-outline
 A script for gimp that allows you to outline.
+This script is maintained under gimp 2.10.
+
+# Installation
+1: You need to know where your gimp configuration folder is.  
+
+ex) /home/xxxxx/snap/gimp/40/.config/GIMP/2.10 <-- ubuntu 18.04  
+
+2: Copy the file called "FU_simple_outline.scm" into "scripts" folder.  
+
+3: Restart gimp.
+
+# Usage
+1: Select a layer that you'd like to add outline.  
+
+2: Open a dialogue box by clicking Script-Fu > simple-outline.  
+
+3: You can choose color, thickness and Feather there.  
+
+4: Click "OK".  
+
+That's all!
+
+# License
+
+This package is licensed under the MIT License.
+
+Copyright 2018 Sukohi Kuhoh
